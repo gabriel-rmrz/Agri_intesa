5cc7122fa18bf0264c6e7ed606e831f325df39e7
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2025-02-13 14:52:03 +0100

    prove of concept working now

--- retrieve_raster_with_wms_v2.py
+++ retrieve_raster_with_wms_v2.py
@@ -1,3 +1,4 @@
+TEST = True
 #import os
 import time
 import urllib
@@ -10,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import cv2
+from pathlib import Path
 from shapely.geometry import shape
 from PIL import Image as PILImage, ImageDraw
 from IPython.display import Image
@@ -24,17 +26,18 @@ def parse_command_line(argv):
   #parser.add_argument('-o','--output_name', help='Name of the output file in geoJSON format', nargs='?', default="output.geojson", type=str)
   return vars(parser.parse_args(argv))
 
-def transform_coord(point_in, crs, res=0.5):
+def transform_coord(point_in, crs_in, crs_out=CRS.from_epsg(3857), res=0.5):
   """
   Calculate image size for a WMS request based on bounding box and max resolution
   """
   # Create CRS object
-  crs_obj = CRS.from_string(crs)
+  crs_obj = CRS.from_string(crs_in)
 
   # Check if the CRS uses degrees (geogrephic) or meters (projected)
   if crs_obj.is_geographic:
     # Transfrom bounding box to a projected CRS for meters
-    transformer = Transformer.from_crs(crs_obj, CRS.from_epsg(3857), always_xy=True)
+    transformer = Transformer.from_crs(crs_obj, crs_out, always_xy=True)
+
     point_out = transformer.transform(point_in[0], point_in[1])
   else:
     # Bounding box is already in projected coordinates (meters)
@@ -42,16 +45,29 @@ def transform_coord(point_in, crs, res=0.5):
 
   return point_out
 
-def get_pixel_coord_from_multipol(pol, crs, res=0.5):
-  points_box = [transform_coord(point, crs) for point in [ (pol.bounds[0], pol.bounds[1]), (pol.bounds[2], pol.bounds[3])]]
+def get_pixel_bounds_from_multipol(multipol, crs, res=0.5):
+  points_box = [transform_coord(point, crs) for point in [ (multipol.bounds[0], multipol.bounds[1]), (multipol.bounds[2], multipol.bounds[3])]]
 
-  points = [ transform_coord(point, crs) for polygon in pol.geoms for point in polygon.exterior.coords[:-1]]
+  points = [ transform_coord(point, crs) for polygon in multipol.geoms for point in polygon.exterior.coords[:-1]]
   point_ref = points_box[0]
   pixel_coord = [ ((int(point[0]/res)-int(point_ref[0]/res)), (int(points_box[1][1]/res) - int(point_ref[1]/res)) - (int(point[1]/res) - int(point_ref[1]/res))) for point in points]
   print(points_box)
   return pixel_coord
 
-def calculate_image_size(bbox, max_resolution, crs):
+def get_pixel_coord_from_pol(pol, crs, res=0.5):
+  print("-----POL-----")
+  print(pol)
+  points = [ transform_coord(point, crs)  for point in pol]
+  point_ref = [np.min(points[:,0]), np.min(points[:,1])]
+  print("-----points-----")
+  print(points)
+  print("-----point_ref-----")
+  print(point_ref)
+  pixel_coord = [ ((int(point[0]/res)-int(point_ref[0]/res)), (int(points_box[1][1]/res) - int(point_ref[1]/res)) - (int(point[1]/res) - int(point_ref[1]/res))) for point in points]
+  print(points_box)
+  return pixel_coord
+
+def calculate_image_size(bounds, max_resolution, crs):
   """
   Calculate image size for a WMS request based on bounding box and max resolution
   """
@@ -62,8 +78,8 @@ def calculate_image_size(bbox, max_resolution, crs):
   if crs_obj.is_geographic:
     # Transfrom bounding box to a projected CRS for meters
     transformer = Transformer.from_crs(crs_obj, CRS.from_epsg(3857), always_xy=True)
-    min_x, min_y = transformer.transform(bbox[0], bbox[1])
-    max_x, max_y = transformer.transform(bbox[2], bbox[3])
+    min_x, min_y = transformer.transform(bounds[0], bounds[1])
+    max_x, max_y = transformer.transform(bounds[2], bounds[3])
   else:
     # Bounding box is already in projected coordinates (meters)
     min_x, min_y, max_x, max_y = bbox
@@ -78,70 +94,82 @@ def calculate_image_size(bbox, max_resolution, crs):
 
   return width, height
 
-def get_images(wms, layer_name, input_data,plot_output=False):
-  for id_parcel, pol in zip(input_data.id, input_data.geometry):
-    crs='EPSG:4326'
-    max_res = 0.2
+def get_image(source_url_wms, source_crs, source_res, province_name, foglio, particella, catasto_bbox, catasto_polygon_pixel, catasto_crs='EPSG:6706', catasto_res=0.02, plot_output=True):
+  catasto_bounds = [catasto_bbox[0][0], catasto_bbox[0][1], catasto_bbox[2][0], catasto_bbox[2][1] ]
+  source_polygon_pixel = (catasto_res*catasto_polygon_pixel/source_res).astype(int)
+  print("----source_polygon_pixel----")
+  print(source_polygon_pixel)
 
-    polygon_coords = get_pixel_coord_from_multipol(pol, crs, max_res)
+  transformer = Transformer.from_crs(catasto_crs, source_crs, always_xy=True)
 
-    print(list(pol.geoms))
-    print(list(pol.bounds))
+  source_bounds_min_x, source_bounds_min_y = transformer.transform(catasto_bounds[0], catasto_bounds[1]) 
+  source_bounds_max_x, source_bounds_max_y = transformer.transform(catasto_bounds[2], catasto_bounds[3]) 
+  #TODO: source bounding box in source_crs
+  source_bounds = [source_bounds_min_x, source_bounds_min_y, source_bounds_max_x, source_bounds_max_y]
 
-    im_size = calculate_image_size(pol.bounds, max_res, crs)
-    print(im_size)
-    img1 = wms.getmap(
-      layers=['0'],
-      #size= [600,400],
-      size= im_size,
-      srs= "EPSG:4326",
-      bbox = list(pol.bounds),
-      #bbox = [14.7434, 40.8638, 15.1123, 41.0615],
-      format= "image/jpeg")
-    print(type(Image(img1)))
+  im_size = [np.max(source_polygon_pixel[:,0]), np.max(source_polygon_pixel[:,1])]
+  print("----im_size----")
+  print(im_size)
+  print("----source_polygon_pixel[:,0]----")
+  print(source_polygon_pixel[:,0])
+  wms = WebMapService(source_url_wms)
+  img1 = wms.getmap(
+    layers=['0'],
+    size= im_size,
+    srs= source_crs,
+    bbox = source_bounds,
+    #bbox = [14.7434, 40.8638, 15.1123, 41.0615],
+    format= "image/jpeg")
+  print(type(Image(img1)))
 
-    Image(img1.read())
-    out_name = f'samples/sample_{layer_name}_id_{id_parcel}'
-    out = open(f'{out_name}.png', 'wb')
-    out.write(img1.read())
+  Image(img1.read())
+  out_name = f'samples/sample_{province_name}_foglio_{foglio}_particella_{particella}'
+  out = open(f'{out_name}.png', 'wb')
+  out.write(img1.read())
 
+  # Convert IPython Image to Numpy array
+  image_data = BytesIO(Image(img1.read()).data)
+  pil_image = PILImage.open(image_data).convert('RGB')
+  image_array = np.array(pil_image)
 
+  # Create a mask
+  mask = np.zeros((image_array.shape[0], image_array.shape[1]), dtype=np.uint8)
+  print("-----image_array.shape-----")
+  print(image_array.shape)
+  mask_image = PILImage.fromarray(mask)
+  draw = ImageDraw.Draw(mask_image)
+  source_polygon_pixel = [(x,y) for x,y in source_polygon_pixel]
+  draw.polygon(source_polygon_pixel, fill = 1)
+  mask = np.array(mask_image)
+  print("-----mask-----")
+  print(mask)
+  print("----source_polygon_pixel----")
+  print(source_polygon_pixel)
 
-    # Convert IPython Image to Numpy array
-    image_data = BytesIO(Image(img1.read()).data)
-    pil_image = PILImage.open(image_data).convert('RGB')
-    image_array = np.array(pil_image)
+  # Apply the mask to the image)
+  masked_image = np.copy(image_array)
+  masked_image[mask!=1] = (0,0,0) # Setmasked pixels to black
 
-    # Create a mask
-    mask = np.zeros((image_array.shape[0], image_array.shape[1]), dtype=np.uint8)
-    mask_image = PILImage.fromarray(mask)
-    draw = ImageDraw.Draw(mask_image)
-    draw.polygon(polygon_coords, fill = 1)
-    mask = np.array(mask_image)
+  if plot_output:
+    # Display the original and masked images
 
-    # Apply the mask to the image)
-    masked_image = np.copy(image_array)
-    masked_image[mask!=1] = (0,0,0) # Setmasked pixels to black
+    fig = plt.figure(figsize=(10,5))
+    ax1 = fig.add_subplot(1,2,1)
+    print(type(ax1))
 
-    if plot_output:
-      # Display the original and masked images
+    ax1.set_title("Original Image")
+    ax1.imshow(image_array)
+    ax1.set_axis_off()
 
-      fig = plt.figure(figsize=(10,5))
-      ax1 = fig.add_subplot(1,2,1)
-      print(type(ax1))
+    ax2 = fig.add_subplot(1,2,2)
+    ax2.set_title("Masked Image")
+    ax2.imshow(masked_image)
+    ax2.set_axis_off()
 
-      ax1.set_title("Original Image")
-      ax1.imshow(image_array)
-      ax1.set_axis_off()
+    #fig.show()
 
-      ax2 = fig.add_subplot(1,2,2)
-      ax2.set_title("Masked Image")
-      ax2.imshow(masked_image)
-      ax2.set_axis_off()
-
-      #fig.show()
-
-      fig.savefig(f'{out_name}_masked.png')
+    fig.savefig(f'{out_name}_masked.png')
+  exit()
 
 def make_bbox_geojson(input_files):
   province_bbox = {}
@@ -190,8 +218,8 @@ def make_bbox_geojson(input_files):
   gdf.to_file('all_bbox_limits.geojson', driver='GeoJSON')
 
 def get_token(max_tries=3):
-  for i in range(max_tries):
-    print("Token request. Try {i}/{max_tries}...")
+  for i in range(1,max_tries+1):
+    print(f"Token request. Try {i}/{max_tries}...")
     url_token = "https://catastomappe.it/get_token.php"
     token_request = urllib.request.urlopen(url_token)
     token = [t for t in token_request]
@@ -200,31 +228,32 @@ def get_token(max_tries=3):
       continue
     token = token[0].decode('utf-8')
     token_json = {
-        "issue_time":datetime.now(),
-        "token": token
+        "token": token,
+        "issued_time": datetime.timestamp(datetime.now())
         }
+    #token_json = list([token_json])
     token_file_name= f"configs/token.json"
-    with open(token_file_name, "rw") as token_file:
-      print(f"Saving token into {token_file_name}."
+    print(f"Saving token into {token_file_name}.")
+    with open(token_file_name, "w") as token_file:
       json.dump(token_json, token_file)
     return token
   return None
 
-def get_image_from_bbox(bbox, crs= 'EPSG:4326', url="https://wms.cartografia.agenziaentrate.gov.it/inspire/wms/ows01.php"):
-    bbox = [bbox[0][0], bbox[0][1], bbox[2][0], bbox[2][1]]
-    print(bbox)
+def get_image_from_bbox(bbox, crs= 'EPSG:4326', res = 0.02,  url="https://wms.cartografia.agenziaentrate.gov.it/inspire/wms/ows01.php"):
+    bounds = [bbox[0][0], bbox[0][1], bbox[2][0], bbox[2][1]]
+
     bb_wms = WebMapService(url) # ,version = "1.3.0") # To add attributes
     print(bb_wms.contents)
     #crs='EPSG:6706'
     max_res = 0.02
-    #polygon_coords = get_pixel_coord_from_multipol(bbox, crs, max_res)
-    im_size = calculate_image_size(bbox, max_res, crs)
+    #polygon_coords = get_pixel_coord_from_multipol(bounds, crs, max_res)
+    im_size = calculate_image_size(bounds, max_res, crs)
     img1 = bb_wms.getmap(
         layers=['CP.CadastralParcel'],
         #size= [600,400],
         size= im_size,
         srs= crs,
-        bbox = list(bbox),
+        bbox = list(bounds),
         #bbox = [14.7434, 40.8638, 15.1123, 41.0615],
         format= "image/jpeg")
     print("...")
@@ -237,10 +266,9 @@ def get_image_from_bbox(bbox, crs= 'EPSG:4326', url="https://wms.cartografia.age
     print("image saved...")
     return img1
 
-#from IPython.core.display import Image as IPImage, display
 
 # Assume 'img_display' is an IPython.core.display.Image object
-def detect_polygons(in_image, output_path="output.jpg", brightness_threshold=80):
+def detect_polygon(in_image, output_path="output.jpg", brightness_threshold=80):
     # Convert IPython Image to NumPy array
     #print(f'detect_polygons: {type(in_image.data)}')
     #img_bytes = BytesIO(in_image)
@@ -289,15 +317,18 @@ def detect_polygons(in_image, output_path="output.jpg", brightness_threshold=80)
 
     # Display the result in Jupyter Notebook
     Image(filename=output_path)
+    print(type(poly))
+    print(poly)
+    return poly
 
-# Example usage:
-# detect_polygons(img_display, "detected_polygons.jpg")
 def make_request(url_request_incomple):
   token = None
-  if file_exist:
-    
-    if time_diff < 300:
-      print("Getting token from file")
+  if Path("configs/token.json").is_file():
+    print("Reading token file.")
+    with open("configs/token.json", 'r') as file:
+      token_from_json = json.load(file)
+    print("Checking token validity")
+    if (datetime.timestamp(datetime.now()) - token_from_json['issued_time']) < 300:
       token = token_from_json['token']
     else:
       print("The token in the file is not longer valid. Trying other options")
@@ -307,14 +338,11 @@ def make_request(url_request_incomple):
     print("Token cannot be issued at this moment. Please retry again later.")
     return None
 
-            
   url_request = url_request_incomple + token
   print("Making request to the the Geoportale Cartografico Catastale")
-
   bbox_request = None
   try:
     bbox_request = urllib.request.urlopen(url_request)
-    print("Request successful!")
   except urllib.error.HTTPError as e:
     print(f"HTTP Error: {e.code} - {e.reason}")
   except urllib.error.URLError as e:
@@ -334,19 +362,37 @@ def get_polygon_from_ae():
   bbox_request = make_request(url_request_incomple)
   if bbox_request == None:
     print("Request cannot be compleated at the moment")
-    return
+    exit()
+    
 
   print(bbox_request)
   geom_all = [json.loads(bb) for bb in bbox_request]
   print(geom_all)
-  geom = json.loads(geom_all[0]['GEOMETRIA'][0])
-  #print(type(geom["coordinates"][0][0]))
-  bbox=geom["coordinates"][0]
-  print(bbox)
+  if TEST:
+    bbox = [ [15.29082657, 41.07571757], [15.29107614, 41.07571757], [15.29107614, 41.07588641], [15.29082657, 41.07588641], [15.29082657, 41.07571757] ]
+  else:
+    try:
+      print(type(geom_all[0]))
+      print(geom_all[0])
+      if isinstance(geom_all[0], str):
+        json_data = json.loads(geom_all[0])
+
+      # Check if the parsed data is a dictionary
+    except json.JSONDecodeError:
+      print("Request cannot be compleated at the moment")
+      exit()
+    geom = json.loads(geom_all[0]['GEOMETRIA'][0])
+    #print(type(geom["coordinates"][0][0]))
+    print("Request successful!")
+    bbox=geom["coordinates"][0]
+    print(bbox)
+
   
   #get_image_from_bbox(bbox,"https://siat2.provincia.avellino.it/server/services/RASTER/Ortofoto_AGEA_2020/MapServer/WMSServer")
-  img_catas = get_image_from_bbox(bbox,'EPSG:6706')
-  detect_polygons(img_catas, "black_polygons.jpg", brightness_threshold=200)
+  img_catas = get_image_from_bbox(bbox,'EPSG:6706', 0.02)
+  polygon = detect_polygon(img_catas, "black_polygons.jpg", brightness_threshold=200)
+  polygon =  polygon.reshape((polygon.shape[0], polygon.shape[2]))
+  return bbox, polygon
 
 def main(argv=None):
   if argv == None:
@@ -356,18 +402,20 @@ def main(argv=None):
   with open(config_file, 'r') as file:
     inputs = yaml.safe_load(file)
   
-  get_polygon_from_ae()
-  exit()
-  wms = WebMapService(inputs['params']['wms']['url'])
-  input_files = [f for f in pathlib.Path().glob("../GEOJSON/FEUDI/GEOJSON_FEUDI/*.geojson")]
+  catasto_bbox, catasto_polygon_pixel = get_polygon_from_ae()
 
-  for in_file in input_files:
-    layer_name = gpd.list_layers(in_file).name[0]
-    input_data = gpd.read_file(in_file, layer=layer_name)
-    print(layer_name)
-    for fog, par in zip(input_data.Foglio, input_data.Particella):
-      print(f"{layer_name}      {fog}         {par}")
-    get_images(wms, layer_name, input_data, plot_output=True)
+  source_url_wms =inputs['params']['wms']['url']
+  source_crs = "EPSG:4326"
+  source_res = 0.2
+  province_name = "province_test"
+  catasto_crs='EPSG:6706'
+  catasto_res = 0.02
+  foglio = 'foglio_test'
+  particella = 'paritcella_test'
+
+
+  get_image(source_url_wms, source_crs, source_res, province_name,foglio, particella, catasto_bbox, catasto_polygon_pixel, catasto_crs, catasto_res, plot_output=True)
+  exit()
 
   make_bbox_geojson(input_files)
 
