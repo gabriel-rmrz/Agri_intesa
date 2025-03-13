24bd3066ef5370532bb1c535d2042241fc3757c1
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2025-03-13 16:46:35 +0100

    v0 now working

--- retrieve_raster_with_wms.py
+++ retrieve_raster_with_wms.py
@@ -49,10 +49,6 @@ def get_images(params, image_name, polygon,plot_output=False):
 
   p, p_pix = get_pixel_coord_from_multipol(polygon, crs_source, res)
   im_size = calculate_image_size(polygon.bounds, res, crs_source)
-  print(im_size)
-
-
-
 
   img1 = wms.getmap(
     layers=params['wms_info']['layer_names'],
@@ -62,7 +58,6 @@ def get_images(params, image_name, polygon,plot_output=False):
     bbox = polygon.bounds,
     #bbox = [14.7434, 40.8638, 15.1123, 41.0615],
     format= params['wms_info']['image_format'])
-  print(type(Image(img1)))
 
   Image(img1.read())
   out_name = f'samples/sample_{image_name}'
@@ -92,7 +87,6 @@ def get_images(params, image_name, polygon,plot_output=False):
 
     fig = plt.figure(figsize=(10,5))
     ax1 = fig.add_subplot(1,2,1)
-    print(type(ax1))
 
     ax1.set_title("Original Image")
     ax1.imshow(image_array)
@@ -102,10 +96,9 @@ def get_images(params, image_name, polygon,plot_output=False):
     ax2.set_title("Masked Image")
     ax2.imshow(masked_image)
     ax2.set_axis_off()
-
     #fig.show()
-
     fig.savefig(f'{out_name}_masked.png')
+
 def round_geom_coords(geom, precision):
     if geom.geom_type == 'Polygon':
         return Polygon(np.round(geom.exterior.coords, precision),
@@ -117,6 +110,32 @@ def round_geom_coords(geom, precision):
     else:
         raise ValueError(f'Geometry type {geom.geom_type} not supported')
 
+def get_label_pixel_coord(polygon_data, polygon_label, params):
+  pass
+def get_label_mask(params, mask_file_name, polygon_data, polygon_intersection):
+  res = params['wms_info']['resolution']
+  p_data = np.array(polygon_data)
+  p_intersection = np.array(polygon_intersection)
+  image_bounds = np.array((float(np.min(p_data[:,0])), float(np.min(p_data[:,1])), float(np.max(p_data[:,0])), float(np.max(p_data[:,1]))))
+  p_intersection_pixel = np.copy(p_intersection)
+  p_intersection_pixel[:,0] = ((p_intersection_pixel[:,0] - image_bounds[0])/res).astype(int)
+  p_intersection_pixel[:,1] = ((p_intersection_pixel[:,1] - image_bounds[1])/res).astype(int)
+  p_intersection_pixel = p_intersection_pixel.astype(int)
+
+  image_bounds_pixel = np.copy(image_bounds)
+  image_bounds_pixel[2] = ((image_bounds_pixel[2] - image_bounds_pixel[0])/res).astype(int)
+  image_bounds_pixel[3] = ((image_bounds_pixel[3] - image_bounds_pixel[1])/res).astype(int)
+  image_bounds_pixel[0] = 0
+  image_bounds_pixel[1] = 0
+  image_bounds_pixel = image_bounds_pixel.astype(int)
+  mask = np.zeros((image_bounds_pixel[2], image_bounds_pixel[3]), dtype=np.uint8)
+  mask_image = PILImage.fromarray(mask)
+  draw = ImageDraw.Draw(mask_image)
+  draw.polygon(p_intersection_pixel, fill=1)
+  mask = np.array(mask_image)
+  print(f"samples/array_mask_{mask_file_name}")
+  np.savez_compressed(f"samples/array_mask_{mask_file_name}", mask)
+
 def main(argv=None):
   if argv == None:
     argv = sys.argv[1:]
@@ -125,6 +144,11 @@ def main(argv=None):
   with open(config_file, 'r') as file:
     inputs = yaml.safe_load(file)
   params = inputs['params']
+  crs_labels =  params['wms_info']['crs']
+  crs_data =  params['requests_file']['crs']
+  crs_meters = CRS.from_epsg(3857)
+  transformer_label = Transformer.from_crs(CRS(crs_labels), crs_meters, always_xy=True)
+  transformer_data = Transformer.from_crs(CRS(crs_data), crs_meters, always_xy=True)
   parcels_file = params['requests_file']['path']
   requests_pd = pd.read_excel(parcels_file,
                              index_col=0,
@@ -139,125 +163,77 @@ def main(argv=None):
   info_comuni_df['norm_comune'] = info_comuni_df['comune'].apply(normalize_string)
 
   for in_file in input_files:
-    print(in_file)
     # Extract filename without path
     filename = str(in_file).rsplit("/", 1)[-1]  # Get last segment after the last "/"
 
     # Remove extension
     result = filename.rsplit(".", 1)[0]
-    print(result.split('.'))
     region, prov, cod_comune, comune = result.split('.')
-    print(region)
     input_data = gpd.read_file(in_file, layer=comune)
     comune_pd = get_df(region,prov, cod_comune, comune)
+
+
     for fog, par, polygon_label in zip(input_data.Foglio, input_data.Particella, input_data.geometry):
+      print(f"foglio: {fog};     particella:{par}")
       if fog is None:
         continue
       fog = re.sub(r'[^0-9]', '', fog)
-      #print(f"{comune}      {fog}         {par}")
-      print(f"comune: {comune}")
       #com_norm = normalize_string(comune) # Decompose accents
       cadastral_id = generate_cadastral_id(cod_comune, int(fog), int(par))
-      print( cadastral_id)
       if not comune_pd.empty:
+        print("Comune non empty")
         polygon_all = comune_pd[comune_pd.gml_id == cadastral_id].geometry
         polygon_all = polygon_all.scale(1.03)
-        precision = 5
+        precision = 6
         polygon_all = polygon_all.apply(lambda geom: round_geom_coords(geom, precision))
         #polygon_label = polygon_label.apply(lambda geom: round_geom_coords(geom, precision))
-        #print(len(polygon_all['coordinates'][0]))
         if len(mapping(polygon_all)['features']) == 0:
           continue
+        print("Geometry present")
         polygon_all_mapped = mapping(polygon_all)['features'][0]['geometry']#['coordinates'][0]
-
-        
-        '''
-        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-
-        print(polygon_all_mapped['type'])
-        if polygon_all_mapped['type'] == 'MultiPolygon':
-          print(polygon_all)
-          print("polygon_all_mapped['coordinates'][0]")
-          print(polygon_all_mapped['coordinates'][0])
-          print(polygon_all_mapped)
-          exit()
-        continue
-        print(polygon_label)
-        print(type(polygon_label))
-        '''
-        points = [Point((np.round(coord[0],5), np.round(coord[1],5))) for poly in polygon_label.geoms for coord in poly.exterior.coords]
-
-
+        #points = [Point((np.round(coord[0],5), np.round(coord[1],5))) for poly in polygon_label.geoms for coord in poly.exterior.coords]
         matches = []
-        
         # Loop over polygon_all to assign index
-        #print(polygon_all
-        #for jdx, (idx, poly) in enumerate(polygon_all.items()):
         polygon_label = make_valid(polygon_label)
+        polygon_label_mapped = mapping(polygon_label)#['features'][0]['geometry']
+
+        if polygon_label_mapped['type'] == 'GeometryCollection':
+          polygon_label_transformed = [transformer_label.transform(c[0],c[1]) for c in polygon_label_mapped['geometries'][0]['coordinates'][0]]
+        else:
+          depth = lambda L: isinstance(L, tuple) and max(map(depth, L))+1
+          if depth(polygon_label_mapped['coordinates'][0]) == 2:
+            polygon_label_mapped = polygon_label_mapped['coordinates'][0]
+          elif depth(polygon_label_mapped['coordinates'][0]) == 3:
+            polygon_label_mapped = polygon_label_mapped['coordinates'][0][0]
+
+          polygon_label_transformed = [transformer_label.transform(c[0],c[1]) for c in polygon_label_mapped]
+
+
         if polygon_all_mapped['type'] == 'MultiPolygon':
           for jdx, poly in enumerate(polygon_all_mapped['coordinates'][0]):
-            poly_intersection = polygon_label.intersection(Polygon(poly))
+            #TODO: Very important to change the coordinates before the intersection!!!!!!!!!!!
+            poly_transformed = [transformer_data.transform(c[0],c[1]) for c in poly]
+            poly_intersection = Polygon(polygon_label_transformed).intersection(Polygon(poly_transformed))
             if not poly_intersection.is_empty:
+              poly_intersection = mapping(poly_intersection)['coordinates'][0]
               matches.append(jdx) 
+              image_name = f"label_region_prov_{comune}_{fog}_{par}_{jdx}"
+              get_label_mask(params, image_name, poly_transformed, poly_intersection)
+              print(image_name)
         else:
           poly = polygon_all_mapped['coordinates'][0]
-          print("polygon_all")
-          print(polygon_all)
-          print("Polygon(points)")
-          print(Polygon(points))
-          print("polygon_all_mapped['type']")
-          print(polygon_all_mapped['type'])
-          poly_intersection = make_valid(polygon_all.item()).intersection(make_valid(Polygon(points)))
-          print(poly_intersection)
-          if not poly_intersection.is_empty:
-            matches.append(0) 
-        '''
-        elif polygon_all_mapped['type'] == 'Polygon':
-          poly = polygon_all_mapped['coordinates'][0]
-          print("polygon_all")
-          print(polygon_all)
-          print("Polygon(points)")
-          print(Polygon(points))
-          print("polygon_all_mapped['type']")
-          print(polygon_all_mapped['type'])
-          poly_intersection = make_valid(polygon_all.item()).intersection(make_valid(Polygon(points)))
-          print(poly_intersection)
-          if not poly_intersection.is_empty:
-            matches.append(0) 
-        '''
-        print(f'matches: {matches}')
+          poly_transformed = [transformer_data.transform(c[0],c[1]) for c in poly]
 
+          #poly_intersection = make_valid(polygon_all.item()).intersection(make_valid(Polygon(points)))
+          poly_intersection = make_valid(Polygon(poly_transformed)).intersection(make_valid(Polygon(polygon_label_transformed)))
+          if not poly_intersection.is_empty:
+            poly_intersection = mapping(poly_intersection)['coordinates'][0]
+            matches.append(0) 
+            image_name = f"label_region_prov_{comune}_{fog}_{par}_0"
+            get_label_mask(params, image_name, poly_transformed, poly_intersection)
           
-        continue
 
-
-        '''
-        print(f"com_norm: {com_norm}")
-        print(f"transform_string(comune): {transform_string(comune)}")
-        print(f"cod_comuni: {info_comuni_df.head(100)}")
-        print(f"cod_comuni: {info_comuni_df.comune.head(100)}")
-        print(f"duplicated: {info_comuni_df[duplicated]}")
-        filtered_comune_df = info_comuni_df[info_comuni_df['norm_comune'].str.contains(com_norm, regex=False)]
-        print(f"filtered_comune_df: {filtered_comune_df}")
-        print(f"filtered_comune_df.prefisso: {filtered_comune_df.prefisso}")
-        
-        if filtered_comune_df.cod_fisco.shape[0] == 1:
-          print(f"str(filtered_comune_df.cod_fisco.item()): {str(filtered_comune_df.cod_fisco.item())}")
-          print('Here')
-        else:
-          print(f"filtered_comune_df.cod_fisco.shape[0]: {filtered_comune_df.cod_fisco.shape[0]}")
-          print(com_norm)
-          print("Ups")
-          print(f"gpd.list_layers(in_file): {gpd.list_layers(in_file)}")
-          print(f"input_data.keys(): {input_data.keys()}")
-          print(f"input_files: {input_files}")
-          print(f"input_data.head(): {input_data.head()}")
-          exit()
-        
-        break
-        '''
-        image_name = f"region_prov_{comune}_{fog}_{par}_{sub_poly}"
-        get_images(params, image_name, polygon_label, plot_output=True)
+    #get_images(params, image_name, polygon_label, plot_output=True)
 
   #make_bbox_geojson(input_files)
 
