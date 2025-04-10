204af05f2d875392bcc018ef840dbdd469e69f2b
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2025-04-10 17:07:27 +0200

    Implementetion of TinySeg model for the classificator

--- binnary_classification_torch.py
+++ binnary_classification_torch.py
@@ -2,7 +2,28 @@ DEBUG = True
 import imageio.v3 as iio
 import numpy as np
 import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
 from torch.nn.functional import pad
+
+
+
+class TinySegNet(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.encoder = nn.Sequential(
+        nn.Conv2d(3, 16,3, padding = 1), nn.ReLU(),
+        nn.Conv2d(16, 32,3, padding=1), nn.ReLU()
+        )
+    self.decoder = nn.Sequential(
+        nn.Conv2d(32, 16,3, padding = 1), nn.ReLU(),
+        nn.Conv2d(16, 1,1) 
+    )
+  def forward(self, x):
+    x = self.encoder(x)
+    x = self.decoder(x)
+    return x
+
 
 def custom_collate_fn(batch):
   images, masks = zip(*batch)
@@ -32,30 +53,45 @@ def custom_collate_fn(batch):
 
 def main():
   device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  input_files = ['Stefano_del_Sole_1_1_0',
+  input_files = ['Santo_Stefano_del_Sole_1_1_0',
                       'Atripalda_4_89_0']
-  batch = []
+  dataset = []
   for in_file in input_files:
       mask_name = f"samples/array_mask_label_region_prov_{in_file}.npz" 
       data_file_name = f"samples/label_region_prov_{in_file}.png"
       data = iio.imread(data_file_name)
-      data = np.tranpose(data, (2,0,1))
-      data = torch.from_numpy(data)
+      data = np.transpose(data, (2,0,1))
+      data = torch.from_numpy(data).float()
       mask = np.load(mask_name)['arr_0']
       mask = mask.reshape((1, mask.shape[0], mask.shape[1]))
-      mask = torch.from_numpy(mask)
-      batch.append((data,mask))
+      mask = torch.from_numpy(mask).float()
+      dataset.append((data,mask))
+
+  loader = DataLoader(dataset, batch_size=2, collate_fn=custom_collate_fn)
+  model = TinySegNet().to(device)
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+  loss_fn = nn.BCEWithLogitsLoss()
+
+  for epoch in range(1):
+    model.train()
+    for images, masks in loader:
+      images = images.to(device)
+      masks = masks.to(device)
+      print(images)
+
+      preds = model(images)
+      loss = loss_fn(preds, masks)
+
+      optimizer.step()
+      print(f"Epoch {epoch+1} loss: {loss.item():.4f}")
 
   if DEBUG:
     print(f"Using device: {device}")
-  batch = [(data, data_labels), (data_2, data_labels_2)]
-  padded = custom_collate_fn(batch)
-  del batch
-  del padded
-  del data
-  del data_labels
-  del data_2
-  del data_labels_2
+  #padded = custom_collate_fn(batch)
+  del dataset
+  #del padded
+  del model
+  del loader
 
 if __name__=='__main__':
   main()
